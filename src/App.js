98a0a607db566e82f1ef0263b3d3a0c0cef5fca5
ldphillips7-ship-it <ldphillips7@gmail.com>
2025-11-20 10:22:57 -0500

    Removed useEffect import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./index.css";
 import { searchLocalPlants, getPlantByName } from "./plantDatabase.js";
 
